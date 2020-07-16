b0d91f725d61b594c90f9eddcfdc839ae7d46e36
Arjun Sreedharan <asreedharan@vmware.com>
2020-07-16 18:12:44 +0000

    detect: always pass & emits a 'dep' providing plan
    Signed-off-by: Arjun Sreedharan <asreedharan@vmware.com>

--- detect.go
+++ detect.go
@@ -4,6 +4,13 @@ import "github.com/paketo-buildpacks/packit"
 
 func Detect() packit.DetectFunc {
 	return func(context packit.DetectContext) (packit.DetectResult, error) {
-		return packit.DetectResult{}, nil
+		return packit.DetectResult{
+			Plan: packit.BuildPlan{
+				Provides: []packit.BuildPlanProvision{
+					{Name: "dep"},
+				},
+				Requires: nil,
+			},
+		}, nil
 	}
 }
--- detect_test.go
+++ detect_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/paketo-buildpacks/packit"
 	"github.com/paketo-buildpacks/dep"
+	"github.com/paketo-buildpacks/packit"
 	"github.com/sclevine/spec"
 
 	. "github.com/onsi/gomega"
@@ -32,13 +32,17 @@ func testDetect(t *testing.T, context spec.G, it spec.S) {
 		Expect(os.RemoveAll(workingDir)).To(Succeed())
 	})
 
-	context("when conditions for detect true are met", func() {
+	context("returns a plan that provides dep", func() {
 		it("detects", func() {
 			result, err := detect(packit.DetectContext{
 				WorkingDir: workingDir,
 			})
 			Expect(err).NotTo(HaveOccurred())
-			Expect(result.Plan).To(Equal(packit.BuildPlan{}))
+			Expect(result.Plan).To(Equal(packit.BuildPlan{
+				Provides: []packit.BuildPlanProvision{
+					{Name: "dep"},
+				},
+			}))
 		})
 	})
 }
